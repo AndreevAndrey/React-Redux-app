f1de1e61444fe1144c08826d99a35e2b794c5869
AndreevAndrey <888Andrei888@mail.ru>
2020-04-26 00:10:11 +0300

    Deleted items saved

--- src/UsersList/UserList.js
+++ src/UsersList/UserList.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import ThreeSixtyIcon from '@material-ui/icons/ThreeSixty';
 import PropTypes from 'prop-types';
 import style from './userList.module.css';
 
 const propTypes = {
   name: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired,
+  id: PropTypes.number,
   shortInfo: PropTypes.string.isRequired,
-  more: PropTypes.string.isRequired,
-  showUser: PropTypes.func.isRequired,
-  deleteItem: PropTypes.func.isRequired,
-  isActive: PropTypes.bool.isRequired,
-  activeId: PropTypes.number
+  more: PropTypes.string,
+  showUser: PropTypes.func,
+  deleteItem: PropTypes.func,
+  isActive: PropTypes.bool,
+  activeId: PropTypes.number,
+  deletedId: PropTypes.number
 };
 
 const defaultProps = {
-  activeId: null
+  activeId: null,
+  id: null,
+  showUser: null,
+  deleteItem: null,
+  isActive: false,
+  more: null,
+  deletedId: null
 };
 
 const UserList = ({
@@ -27,20 +35,31 @@ const UserList = ({
   showUser,
   deleteItem,
   isActive,
-  activeId
+  activeId,
+  deletedId
 }) => {
   return (
     <div className={style.userList}>
       <div
-        className={isActive && activeId === id ? style.active : ''}
-        onClick={() => showUser(id, more, shortInfo, name)}
+        className={
+          isActive && activeId === id
+            ? style.active
+            : deletedId
+            ? style.deletedUser
+            : ''
+        }
+        onClick={() => (deletedId ? '' : showUser(id, more, shortInfo, name))}
       >
         <p>Name: {name}</p>
         <p>Short info: {shortInfo}</p>
       </div>
-      <IconButton aria-label='delete' onClick={() => deleteItem(id)}>
-        <DeleteIcon />
-      </IconButton>
+      {deletedId ? (
+        <ThreeSixtyIcon />
+      ) : (
+        <IconButton aria-label='delete' onClick={() => deleteItem(id)}>
+          <DeleteIcon />
+        </IconButton>
+      )}
     </div>
   );
 };
